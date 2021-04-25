ab231847ddba10f01346b8dc6b56a19b45bc0bf0
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2021-04-25 13:57:22 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,24 +1,24 @@
-import random
-from enum import IntEnum
+def get_phrase():
+    phrase =  input("Choose phrase: ")
+    phrase_result = [s for s in hashtag_all if phrase in s]
+    return phrase_result
 
 
-def choose_phrase():
-    phrase = input("Which phrase are you looking for?: ")
-    hashtagsOutput = [s for s in hashtagsUnique if phrase in s]
-    return hashtagsOutput
-
-
-def how_many_hashtags(hashtagsOutput):
+def get_number(phrase_result):
+    import random
+    
     try:
-        numberOfHashtags = int(input("How many hashtags do you need?: "))
-        hashtagsChosen = random.sample(hashtagsOutput, numberOfHashtags)
+        hashtag_num = int(input("How many hashtags do you need?: "))
+        hashtag_set = random.sample(phrase_result, hashtag_num)
+    
     except ValueError:
-        print("This Hashtag has only", len(hashtagsOutput), "positions. They have been added.")
-        hashtagsChosen = random.sample(hashtagsOutput, len(hashtagsOutput))
-        for hashtag in hashtagsChosen:
-            hashtagList.append(hashtag)
-        return len(hashtagsOutput)
+        print("This Hashtag has only", len(phrase_result), "positions. They have been added.")
+        hashtag_set = random.sample(phrase_result, len(phrase_result))
+        for hashtag in hashtag_set:
+            export_list.append(hashtag)
+        return len(phrase_result)
+    
     else:
-        for hashtag in hashtagsChosen:
-            hashtagList.append(hashtag)
-        return numberOfHashtags
+        for hashtag in hashtag_set:
+            export_list.append(hashtag)
+        return hashtag_num
