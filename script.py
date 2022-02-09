5ee52192f3ff55d34c8f0f996f2615374f8161d0
Kuba <75746226+Whisperer8526@users.noreply.github.com>
2022-02-09 16:13:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,33 @@
 from enum import IntEnum
 import os
+import random
+
 folder = r"‪C:\Users\Lenovo\Desktop\Python\Hashtag Generator"
 folder = folder.lstrip("\u202a")
+
+def get_phrase():
+    phrase =  input("Choose phrase: ")
+    phrase_result = [s for s in hashtag_all if phrase in s]
+    return phrase_result
+
+
+def get_number(phrase_result):
+        
+    try:
+        hashtag_num = int(input("How many hashtags do you need?: "))
+        hashtag_set = random.sample(phrase_result, hashtag_num)
+    
+    except ValueError:
+        print("This Hashtag has only", len(phrase_result), "positions. They have been added.")
+        hashtag_set = random.sample(phrase_result, len(phrase_result))
+        for hashtag in hashtag_set:
+            export_list.append(hashtag)
+        return len(phrase_result)
+    
+    else:
+        for hashtag in hashtag_set:
+            export_list.append(hashtag)
+        return hashtag_num
 
 
 with open(os.path.join(folder, "Hash.txt"), "r", encoding="UTF-8") as file:
